d701e23985fc9c84107344589b67b634c60dbe2e
Attila Katona <katona.skmobil@gmail.com>
2019-11-28 23:28:19 +0100

    Create gyik.php

---
+++ gyik.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+$arr1 = [
+    ['tegnapelott','10'],
+    ['tegnap','11'],
+    ['ma','12'],
+    ['holnaputan','13']
+];
+
+$arr2 = [
+    ['tegnapelott','1'],
+    ['tegnap','2'],
+    ['holnap','3'],
+    ['holnaputan','4']
+];
+
+$arr3 = [
+    ['jovoheten', '9'], 
+];
+
+$arr4 = [
+    ['ma', '14'], 
+];
