88e792ce9f7fd2f19f5116e11e084fd116ee3711
Attila Katona <katona.skmobil@gmail.com>
2019-12-03 20:13:36 +0100

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+const numbers = [12, 45, 1, 43, 8, 2, 99, 85, 77, 63, 19, 2];
+
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (i = 0; i < length; i++){
+    for(j = 0; j < length; j++) {
+      if (array[j] > array[j+1]) {
+        //swap numbers
+      }
+      console.log(array[j])
+    }
+  }
+}
+
+bubbleSort(numbers);
+
+
