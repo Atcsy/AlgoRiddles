58f25de07fa8a8ddfb36a2c21ff23252987f6c1c
Attila Katona <katona.skmobil@gmail.com>
2019-12-11 14:53:55 +0100

    Create JsInterview1.js

---
+++ JsInterview1.js
@@ -0,0 +1,44 @@
+const inventory = [
+  {
+    id: 1,
+    owner: 1,
+    name: 'Sword',
+    weight: 10,
+    value: 100
+  },
+  {
+    id: 2,
+    owner: 1,
+    name: 'Shield',
+    weight: 10,
+    value: 100
+  },
+  {
+    id: 3,
+    owner: 2,
+    name: 'Sword',
+    weight: 10,
+    value: 100
+  }
+];
+
+const characters = [
+  {
+    id: 1,
+    name: 'Steve',
+    points: 500,
+    level: 5,
+  },
+  {
+    id: 2,
+    name: 'Ron',
+    points: 200,
+    level: 2,
+  },
+  {
+    id: 3,
+    name: 'Jack',
+    points: 0,
+    level: 1,
+  }
+]
