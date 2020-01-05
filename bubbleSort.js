f712d58b1f6873c8ddffff40799db8f7c6202d9d
Attila Katona <katona.skmobil@gmail.com>
2020-01-05 23:45:19 +0100

    swap numbers with a temp variable

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,17 +7,21 @@ function bubbleSort(array) {
     for(j = 0; j < length; j++) {
       if (array[j] > array[j+1]) {
         //swap numbers
+        let temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
       }
-      console.log(array[j])
+      //console.log(array[j])
     }
   }
 }
 
 bubbleSort(numbers);
+console.log(numbers);
 
 
 /*
 Expected result:
-  sorted [12, 45, 1, 43, 8, 2, 99, 85, 77, 63, 19, 2]
+[ 1, 2, 2, 8, 12, 19, 43, 45, 63, 77, 85, 99 ]
 */
 
